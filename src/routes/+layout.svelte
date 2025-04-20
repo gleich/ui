333fa5208c102f97ff10f10f96a848eb1f6e23ad
Matt Gleich <git@mattglei.ch>
2025-04-20 13:42:29 -0400

    feat: basic title
    Signed-off-by: Matt Gleich <git@mattglei.ch>

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import '../lib/ui.css';
+
+	const { children }: { children: Snippet } = $props();
+</script>
+
+<div class="container">
+	<div class="main">
+		{@render children()}
+	</div>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		min-height: 100vh;
+		overflow: hidden;
+	}
+
+	.main {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		flex: 1;
+		max-width: 1000px;
+		padding: 0 20px;
+		padding-bottom: 0;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,31 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Logo from '$lib/logo.svelte';
+
+</script>
+
+<div class="title">
+<div class="logo">
+    <Logo/>
+</div>
+
+<h1>gleich/ui</h1>
+
+</div>
+
+<style>
+    .logo {
+        width: 60px;
+        height: 60px;
+        border: 1px solid var(--border);
+        box-shadow: var(--box-shadow);
+        border-radius: var(--border-radius);
+        background-color: var(--background);
+        padding: 3px;
+    }
+
+    .title {
+        display: flex;
+        gap: 20px;
+        align-items: center;
+    }
+</style>

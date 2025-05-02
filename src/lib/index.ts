56844f184e991a32d08965ec483061737086adf7
Matt Gleich <git@mattglei.ch>
2025-05-02 12:36:42 -0400

    feat[release]: 1.1.1
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,9 +2,10 @@ import Card from './card.svelte';
 import Copyright from './copyright.svelte';
 import DynamicHead from './dynamic-head.svelte';
 import Error from './error.svelte';
+import FadeUp from './fade-up.svelte';
 import Image from './image.svelte';
 import Logo from './logo.svelte';
 import NavLogo from './nav-logo.svelte';
 import Scrolling from './scrolling.svelte';
 
-export { Card, Copyright, DynamicHead, Error, Image, Logo, NavLogo, Scrolling };
+export { Card, Copyright, DynamicHead, Error, FadeUp, Image, Logo, NavLogo, Scrolling };

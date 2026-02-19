5b56df8fe5371110850037b3e9d6b556f6cc7d3b
Matt Gleich <git@mattglei.ch>
2026-02-19 15:18:51 -0500

    feat: export OpenGraphImage
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 import Card from './card.svelte';
 import Copyright from './copyright.svelte';
-import DynamicHead from './dynamic-head.svelte';
+import DynamicHead, { type OpenGraphImage } from './dynamic-head.svelte';
 import Error from './error.svelte';
 import Image from './image.svelte';
 import Layout from './layout.svelte';
@@ -9,4 +9,16 @@ import NavLogo from './nav-logo.svelte';
 import Scrolling from './scrolling.svelte';
 import { siteName } from './sitename.js';
 
-export { siteName, Card, Copyright, DynamicHead, Error, Image, Layout, Logo, NavLogo, Scrolling };
+export {
+	siteName,
+	Card,
+	Copyright,
+	DynamicHead,
+	type OpenGraphImage,
+	Error,
+	Image,
+	Layout,
+	Logo,
+	NavLogo,
+	Scrolling
+};
